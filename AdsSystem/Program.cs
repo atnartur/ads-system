fb03a79edaa92046392c5af17cb1ad3f13bead60
atnartur <i@atnartur.ru>
2017-11-28 21:22:59 +0300

    ! kestrel listen all interfaces

--- AdsSystem/Program.cs
+++ AdsSystem/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using AdsSystem.Models;
-using HandlebarsDotNet;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
